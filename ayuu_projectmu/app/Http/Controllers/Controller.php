9ab9b7406322cf16fea46f537322b0e6979903ae
ayupramudita6 <ayupamudita63@gmail.com>
2025-02-10 16:01:13 +0700

    Update Controller.php

--- ayuu_projectmu/app/Http/Controllers/Controller.php
+++ ayuu_projectmu/app/Http/Controllers/Controller.php
@@ -1,8 +1,17 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Http\Request;
 
-abstract class Controller
+class UserController extends Controller
 {
-    //
+    public function index()
+    {
+        return "Ini adalah halaman User";
+    }
+
+    public function show()
+    {
+        return "Ini adalah halaman profil user6.";
+    }
 }
