7acfb75a3872f2333b8e3757bbeab55d9a396986
ayupramudita6 <ayupamudita63@gmail.com>
2025-02-10 16:01:24 +0700

    Create UserProfileController.php

---
+++ ayuu_projectmu/app/Http/Controllers/UserProfileController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+class UserProfileController extends controller
+{
+    public function index()
+{
+    return "ini adalah halaman user";
+}
+
+public function show()
+{
+    return "ini adalah halaman profil user6";
+}
+}
