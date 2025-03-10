8e4c8a015c22f0afd9b6a6567ef736f2dc08e880
ayupramudita6 <ayupamudita63@gmail.com>
2025-03-10 21:20:47 +0700

    Update PendidikanController.php

--- ayuu_projectmu/app/Http/Controllers/PendidikanController.php
+++ ayuu_projectmu/app/Http/Controllers/PendidikanController.php
@@ -6,5 +6,60 @@ use Illuminate\Http\Request;
 
 class PendidikanController extends Controller
 {
-    //
+    public function index()
+    {
+        $pendidikan = Pendidikan::all();
+        return view('backend.pendidikan.index', compact('pendidikan'));
+    }
+
+    public function create()
+    {
+        return view('backend.pendidikan.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nama' => 'required|string|max:255',
+            'tingkatan' => 'required|string|max:10',
+            'tahun_masuk' => 'required|integer',
+            'tahun_keluar' => 'required|integer',
+        ]);
+
+        Pendidikan::create($request->all());
+
+        return redirect()->route('pendidikan.index')
+            ->with('success', 'Data Pendidikan baru berhasil disimpan.');
+    }
+
+    public function edit($id)
+    {
+        $pendidikan = Pendidikan::findOrFail($id);
+        return view('backend.pendidikan.edit', compact('pendidikan'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'nama' => 'required|string|max:255',
+            'tingkatan' => 'required|string|max:10',
+            'tahun_masuk' => 'required|integer',
+            'tahun_keluar' => 'required|integer',
+        ]);
+
+        $pendidikan = Pendidikan::findOrFail($id);
+        $pendidikan->update($request->all());
+
+        return redirect()->route('pendidikan.index')
+            ->with('success', 'Data Pendidikan berhasil diperbarui.');
+    }
+
+    public function destroy($id)
+    {
+        $pendidikan = Pendidikan::findOrFail($id);
+        $pendidikan->delete();
+
+        return redirect()->route('pendidikan.index')
+            ->with('success', 'Data Pendidikan berhasil dihapus.');
+    }
 }
