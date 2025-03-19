e60c06f5782d32359e8bdcc5892ad94c5b3e038e
ayupramudita6 <ayupamudita63@gmail.com>
2025-03-19 07:26:52 +0700

    Create ApiPendidikanController.php

---
+++ ayuu_projectmu/app/Http/Controllers/backend/ApiPendidikanController.php
@@ -0,0 +1,35 @@
+<?php
+namespace App\Http\Controllers\Backend;
+
+use App\Models\Pendidikan;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Response;
+
+class ApiPendidikanController extends Controller
+{
+    //Acara 21
+    public function getAll()
+    {
+        $pendidikan = Pendidikan::all();
+        return Response::json($pendidikan, 201);
+    }
+
+    //Acara 22
+    public function getPen($id)
+{
+    $pendidikan = Pendidikan::find($id);
+
+    return Response::json($pendidikan, 200);
+}
+    public function createPen(Request $request)
+    {
+        Pendidikan::create($request->all());
+
+        return response()->json([
+            'status' => 'ok',
+            'message' => 'Pendidikan berhasil ditambahkan!'
+        ], 201);
+    }
+
+}
